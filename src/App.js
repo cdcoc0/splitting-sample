5baef004b56aed8733451b1a955d3521563cd20c
velykitty <jiyunee13@gmail.com>
2021-05-16 21:37:31 +0900

    Preload Component -Loadable Components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, {useState, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
 //import notify from './notify';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+//const SplitMe = React.lazy(() => import('./SplitMe'));
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
@@ -10,16 +14,19 @@ function App() {
     setVisible(true);
   };
 
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          {/* 마우스를 커서에 올리면 로딩 시작, 클릭했을 때 바로 렌더링 */}
           Hello React!
         </p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
